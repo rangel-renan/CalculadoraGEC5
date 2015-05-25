610dfcc6aae00eeb3caf4566721d1e29a48dd96f
RenanMOliveira1 <renan.oliveira_10@hotmail.com>
2015-05-25 20:38:31 -0300

    1.1
    termino da criação da classe bhaskara afim de ser condicionado o teste

--- src/com/calculadora/formulas/Bhaskara.java
+++ src/com/calculadora/formulas/Bhaskara.java
@@ -1,9 +1,14 @@
 package com.calculadora.formulas;
 
+import java.util.Scanner;
+
 public class Bhaskara {
 	private double a;
 	private double b;
 	private double c;
+	private double delta;
+	private double resultPositivo;
+	private double resultNegativo;
 	
 	public double getA() {
 		return a;
@@ -24,7 +29,69 @@ public class Bhaskara {
 		this.c = c;
 	}
 	
-	public void formulaBhaskara(){
+	public double getDelta() {
+		return delta;
+	}
+	public void setDelta(double delta) {
+		this.delta = delta;
+	}
 		
+	public double getResultPositivo() {
+		return resultPositivo;
+	}
+	
+	public void setResultPositivo(double resultPositivo) {
+		this.resultPositivo = resultPositivo;
+	}
+	
+	public double getResultNegativo() {
+		return resultNegativo;
+	}
+	
+	public void setResultNegativo(double resultNegativo) {
+		this.resultNegativo = resultNegativo;
+	}
+	
+	public void fazdelta(){
+		
+		double quadradoB = getB() * getB();
+		this.setDelta(quadradoB - (4 * getA() * getB()));
+	}
+	
+	public void levalores(){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Entre com o valor de a");
+		this.setA(input.nextDouble());
+		System.out.println("Entre com o valor de b");
+		this.setA(input.nextDouble());
+		System.out.println("Entre com o valor de c");
+		this.setA(input.nextDouble());
+		input.close();
+	}
+	
+	public void acharResultPositivo(){
+		setResultPositivo( ( -( getB() ) + ( Math.sqrt(delta) ) ) / 2 * getA());
+	}
+	
+	public void acharResultNegativo(){
+		setResultNegativo( ( -( getB() ) - ( Math.sqrt(delta) ) ) / 2 * getA());
+	}
+	
+	public void MostraResultados(){
+		System.out.println("x1 = " + getResultPositivo());
+		System.out.println("x2 = " + getResultNegativo());
+	}
+	
+	public void formulaBhaskara(){
+		this.levalores();
+		this.fazdelta();
+		if(getDelta() < 0){
+			System.out.println("Delta com valor " + getDelta() + "Impossivel continuar a expressao");
+		} else {
+			this.acharResultPositivo();
+			this.acharResultNegativo();
+			this.MostraResultados();
+		}
 	}
 }
