e86032260fcea9ce0759e84beec7502d8614f5cd
tiagohs <tiago.silva.93@hotmail.com>
2015-05-25 19:54:02 -0300

    teste pelo eclipse

---
+++ src/com/calculadora/operacoes/OperacoesBasicasTeste.java
@@ -0,0 +1,12 @@
+package com.calculadora.operacoes;
+
+public class OperacoesBasicasTeste {
+
+	public static void main(String[] args) {
+		
+		System.out.println(OperacoesBasicas.operacaodeSoma(5, 7));
+		System.out.println(OperacoesBasicas.operacaodeSubtracao(5, 7));
+		System.out.println(OperacoesBasicas.operacaodeMultiplicacao(5, 7));
+		System.out.println(OperacoesBasicas.operacaodeDivisao(4, 2));
+	}
+}
