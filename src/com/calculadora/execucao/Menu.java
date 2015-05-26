afb827980c6163967151e109175c9e3e07eff202
RenanMOliveira1 <renan.oliveira_10@hotmail.com>
2015-05-26 02:37:09 -0300

    Menu v1.1
    Menu com a adição da chamada do metodo para a formula de bhaskara.
    Existe um erro quando o menu e percorrido pela segunda vez, que ira ser
    consertado em modificaões futuras

--- src/com/calculadora/execucao/Menu.java
+++ src/com/calculadora/execucao/Menu.java
@@ -2,29 +2,36 @@ package com.calculadora.execucao;
 
 import java.util.Scanner;
 
+import com.calculadora.formulas.Bhaskara;
+
 public class Menu {
-	public static final int BHASKARA = 2;
 	public static final int CALCULADORA = 1;
-	
-	public static void main(){
-		
+	public static final int BHASKARA = 2;
+
+	public static void main(String Args[]){
+
 		boolean sair = false;
-		int opcao;
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		while(!sair){
+			int opcaoMenu;
 			System.out.println("Entre com a opção desejada: ");
 			System.out.println("1-Calculadora");
 			System.out.println("2-Bhaskara");
 			System.out.println("0-Sair");
-			opcao = input.nextInt();
-			switch(opcao){
-				case 0:
-					sair = true;
-					break;
+			opcaoMenu = Integer.parseInt(input.next());
+			switch(opcaoMenu){
+			case BHASKARA:
+				Bhaskara bhaskara = new Bhaskara();
+				bhaskara.formulaBhaskara();
+				break;
+			case 0:
+				sair = true;
+				break;
 			}
+		input.close();
 		}
-	input.close();
+		System.out.println("Calculadora finalizada!!!");
 	}
 }
