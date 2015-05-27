7a26f3b7aec1b627fd53ebe6af0fc10a6eb6acaa
Yasmin Farias Romi de Sant'Anna <yasmin.romi@al.infnet.edu.br>
2015-05-27 19:29:35 -0300

    Menu v1.4
    Adicionada a opção "calculo do módulo do vetor".

--- src/com/calculadora/execucao/Menu.java
+++ src/com/calculadora/execucao/Menu.java
@@ -3,12 +3,14 @@ package com.calculadora.execucao;
 import java.util.Scanner;
 
 import com.calculadora.formulas.Bhaskara;
+import com.calculadora.vetores.ModuloVetor;
 import com.calculadora.vetores.ProdutoEscalar;
 
 public class Menu {
 	public static final int CALCULADORA = 1;
 	public static final int BHASKARA = 2;
 	public static final int PRODUTO_ESCALAR = 3;
+	public static final int MODULO_VETOR = 4;
 
 	public static void main(String Args[]){
 
@@ -22,6 +24,7 @@ public class Menu {
 			System.out.println("1-Calculadora");
 			System.out.println("2-Bhaskara");
 			System.out.println("3-Produto Escalar de 2 vetores");
+			System.out.println("4-Modulo de um vetor");
 			System.out.println("0-Sair");
 			System.out.print("Entre com a opção desejada: ");
 			opcaoMenu = Integer.parseInt(input.nextLine());
@@ -34,6 +37,9 @@ public class Menu {
 				ProdutoEscalar produto = new ProdutoEscalar();
 				produto.produto();
 				break;
+			case MODULO_VETOR:
+				ModuloVetor modulo = new ModuloVetor();
+				modulo.calculaModulo();
 			case 0:
 				sair = true;
 				break;
