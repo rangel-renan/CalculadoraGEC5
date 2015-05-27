2d43fefd1033396c2baa7d95075f81561564908b
RenanMOliveira1 <renan.oliveira_10@hotmail.com>
2015-05-27 17:50:34 -0300

    Menu v1.3
    Correção do erro presente no Menu, e adição do produto escalar

--- src/com/calculadora/execucao/Menu.java
+++ src/com/calculadora/execucao/Menu.java
@@ -3,22 +3,25 @@ package com.calculadora.execucao;
 import java.util.Scanner;
 
 import com.calculadora.formulas.Bhaskara;
+import com.calculadora.vetores.ProdutoEscalar;
 
 public class Menu {
 	public static final int CALCULADORA = 1;
 	public static final int BHASKARA = 2;
+	public static final int PRODUTO_ESCALAR = 3;
 
 	public static void main(String Args[]){
 
-		//boolean sair = false;
+		boolean sair = false;
 
 		Scanner input = new Scanner(System.in);
 
-		//while(!sair){
+		while(!sair){
 			int opcaoMenu;
 			System.out.println("********MENU********");
 			System.out.println("1-Calculadora");
 			System.out.println("2-Bhaskara");
+			System.out.println("3-Produto Escalar de 2 vetores");
 			System.out.println("0-Sair");
 			System.out.print("Entre com a opção desejada: ");
 			opcaoMenu = Integer.parseInt(input.nextLine());
@@ -27,12 +30,16 @@ public class Menu {
 				Bhaskara bhaskara = new Bhaskara();
 				bhaskara.formulaBhaskara();
 				break;
+			case PRODUTO_ESCALAR:
+				ProdutoEscalar produto = new ProdutoEscalar();
+				produto.produto();
+				break;
 			case 0:
-				//sair = true;
+				sair = true;
 				break;
 			}
+		}
 		input.close();
-		//}
 		System.out.println("Calculadora finalizada!!!");
 	}
 }
