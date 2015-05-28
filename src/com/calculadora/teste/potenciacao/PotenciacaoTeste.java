6de48b94ec56831ece9072c6be9e060b6c687e5f
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-05-28 19:57:47 -0300

    Potencialização v1.0

---
+++ src/com/calculadora/teste/potenciacao/PotenciacaoTeste.java
@@ -0,0 +1,14 @@
+package com.calculadora.teste.potenciacao;
+
+import com.calculadora.potenciacao.Potenciacao;
+
+public class PotenciacaoTeste {
+
+	public static void main(String[] args) {
+		
+		Potenciacao potencia = new Potenciacao();
+		
+		potencia.resultadoPotencial();
+		
+	}
+}
