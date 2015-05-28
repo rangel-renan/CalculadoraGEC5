0254223d2a47720a7fe8a857ae14bb3e257549dd
RenanMOliveira1 <renan.oliveira_10@hotmail.com>
2015-05-28 18:16:39 -0300

    Determinante Teste v1.0
    criação da classe de teste para a determinante

---
+++ src/com/calculadora/teste/matriz/DeterminanteTeste.java
@@ -0,0 +1,13 @@
+package com.calculadora.teste.matriz;
+
+import com.calculadora.matriz.Determinante;
+
+public class DeterminanteTeste {
+
+	public static void main(String[] args) {
+		
+	Determinante determinante = new Determinante();
+	
+	}
+
+}
