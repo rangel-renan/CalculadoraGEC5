a63b90b691346e09574a48daf68d563b4364f053
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-05-28 19:58:39 -0300

    Menu v1.8

--- src/com/calculadora/execucao/Menu.java
+++ src/com/calculadora/execucao/Menu.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import com.calculadora.calculadora.OperacoesBasicasMenu;
 import com.calculadora.formulas.Bhaskara;
 import com.calculadora.formulas.CossenoEntreVetores;
+import com.calculadora.potenciacao.Potenciacao;
 import com.calculadora.raizquadra.RaizQuadrada;
 import com.calculadora.vetores.ModuloVetor;
 import com.calculadora.vetores.ProdutoEscalar;
@@ -16,6 +17,7 @@ public class Menu {
 	public static final int MODULO_VETOR = 4;
 	public static final int COSSENO_ENTRE_VETORES = 5;
 	public static final int RAIZ_QUADRADA = 6;
+	public static final int POTENCIACAO = 7;
 
 	public static void main(String Args[]){
 
@@ -26,12 +28,13 @@ public class Menu {
 		while(!sair){
 			int opcaoMenu;
 			System.out.println("********MENU********\n");
-			System.out.println("1-Calculadora");
-			System.out.println("2-Bhaskara");
-			System.out.println("3-Produto Escalar de 2 vetores");
-			System.out.println("4-Modulo de um vetor");
-			System.out.println("5-Cosseno entre vetores");
-			System.out.println("6-Raiz Quadrada de um numero");
+			System.out.println("1- Calculadora");
+			System.out.println("2- Bhaskara");
+			System.out.println("3- Produto Escalar de 2 vetores");
+			System.out.println("4- Modulo de um vetor");
+			System.out.println("5- Cosseno entre vetores");
+			System.out.println("6- Raiz Quadrada de um numero");
+			System.out.println("7- Potencialização");
 			System.out.println("0-Sair");
 			System.out.print("\nEntre com a opção desejada: ");
 			opcaoMenu = Integer.parseInt(input.nextLine());
@@ -59,6 +62,10 @@ public class Menu {
 			case RAIZ_QUADRADA:
 				RaizQuadrada raizquadrada = new RaizQuadrada();
 				raizquadrada.raizQuadrada();
+				break;
+			case POTENCIACAO:
+				Potenciacao potenciacao = new Potenciacao();
+				potenciacao.resultadoPotencial();
 				break;
 			case 0:
 				sair = true;
