5b70581d8d3eee76e5e67f5b647edab886869874
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-05-28 18:08:32 -0300

    Raiz Quadrada v1.0
    Exibe o resultado de uma raiz.

---
+++ src/com/calculadora/raizquadra/RaizQuadrada.java
@@ -0,0 +1,33 @@
+package com.calculadora.raizquadra;
+
+import java.util.Scanner;
+
+public class RaizQuadrada {
+	
+	private int number;
+	private int resultadoRaiz;
+	private Scanner input = new Scanner(System.in);
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public double raizQuadrada(){			
+		
+		System.out.print("Entre com o numero: ");
+		number = input.nextInt();
+		
+		resultadoRaiz = (int) Math.sqrt(number);
+		
+		System.out.println("Resultado da Raiz: " + resultadoRaiz);
+		
+		return resultadoRaiz;
+	}
+	
+	
+
+}
