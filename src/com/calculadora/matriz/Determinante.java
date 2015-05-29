2023de1aed1e9443895419b8ffd117de228b2af5
Ramon Portela <portela.ramon96@gmail.com>
2015-05-29 01:46:03 -0300

    Determinante v1.0

--- src/com/calculadora/matriz/Determinante.java
+++ src/com/calculadora/matriz/Determinante.java
@@ -2,12 +2,15 @@ package com.calculadora.matriz;
 
 import java.util.Scanner;
 
-public class Determinante {
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
-	private double[][] matriz;
-	private double determinante;
+public class Determinante {
 	private int numeroLinhas;
 	private int numeroColunas;
+	
+	private double[][] matriz;
+	private double determinante;
+
 	
 	public double[][] getMatriz() {
 		return matriz;
@@ -40,27 +43,89 @@ public class Determinante {
 		
 		return Integer.parseInt(input.nextLine());
 	}
-
-	public double[] lelinha(int numeroLinha){
-		double[] linha = new double[(int) this.getNumeroColunas()];
+	public boolean leMatriz(){
+		setNumeroLinhas(LeMatriz.leLinhas());
+		setNumeroColunas(LeMatriz.leColunas());
+		int numLinhas = getNumeroLinhas();
+		int numColunas = getNumeroColunas();
 		
-		for(int contador = 0; contador < getNumeroColunas(); contador++){
-			System.out.print("Entre com o numero da linha " + numeroLinha + " e Coluna " + (contador + 1) + ": ");
-			linha[contador] = this.lenum();
+		if(numLinhas != numColunas){
+			System.out.println("Não possui determinante");
+			return false;
+		}else{
+			setMatriz(LeMatriz.leMatriz(getNumeroLinhas(), getNumeroColunas()));
+			return true;
 		}
-		return linha;
+		
 	}
 	
-	public void calculaDeterminante(){
-		double[][] matriz;
-		
-		System.out.print("Entre com o numero de linhas da matriz: ");
-		this.setNumeroLinhas(this.lenum());
-		System.out.print("Entre com o numero de coluas da matriz: ");
-		this.setNumeroColunas(this.lenum());
-		double[] linha1 = this.lelinha(1);
-		double[] linha2 = this.lelinha(2);
-		double[] linha3 = new double[(int) getNumeroColunas()];
-		
+	public double[] determinanteMatriz2(int numeroLinhas, int numeroColunas, double[][] matrizLocal){
+		double[] vetorRetorno = new double[2];
+		int verificacao = 0;
+		double determinante = 0;
+		double multiplicacao = 1;
+		double somaDiagonalPrincipal = 0;
+		double somaDiagonalInversa = 0;
+
+		if(numeroLinhas != numeroColunas){
+			System.out.println("Não é possivel tirar determinante.");
+			vetorRetorno[0] = verificacao;
+			vetorRetorno[1] = determinante;
+			return vetorRetorno;
+		}
+		else{
+			for(int numLinhas = 0; numLinhas < numeroLinhas; numLinhas++){
+				for(int numColunas = 0; numColunas < numeroColunas; numColunas++){
+					int auxDeterminante = (numLinhas + numColunas) % numeroLinhas;
+					multiplicacao *= matrizLocal[numColunas][auxDeterminante];
+				}
+				somaDiagonalPrincipal += multiplicacao;
+			}
+			multiplicacao = 1;
+			for(int numLinhas = (numeroLinhas-1); numLinhas >= 0; numLinhas--){
+				for(int numColunas = (numeroColunas-1); numColunas >= 0; numColunas--){
+					int auxDeterminante = (numLinhas + numColunas) % numeroLinhas;
+					multiplicacao *= matrizLocal[numColunas][auxDeterminante];
+				}
+			somaDiagonalInversa+= multiplicacao;
+			}
+			determinante = somaDiagonalPrincipal - somaDiagonalInversa;
+			verificacao = 1;
+			vetorRetorno[0] = verificacao;
+			vetorRetorno[1] = determinante;
+			return vetorRetorno;
+		}
 	}
+
+	public double determinante(){
+		
+		
+		double determinante;		
+		int numeroLinhas;
+		int numeroColunas;
+		double[] vetorDet;
+		
+		if(leMatriz()){
+			numeroLinhas = getNumeroLinhas();
+			numeroColunas = getNumeroColunas();
+			double[][] matrizLocal = getMatriz();
+			if(numeroLinhas == 1){
+				determinante = matrizLocal[0][0];
+				return determinante;
+			}else{
+				while(numeroLinhas > 2){
+					//reduzMatriz(matrizLocal);
+					numeroLinhas--;
+					numeroColunas--;
+				}
+				vetorDet = determinanteMatriz2(numeroLinhas, numeroColunas, matrizLocal);
+				if(vetorDet[0] == 1){
+					determinante = vetorDet[1];
+					System.out.println(determinante);
+					return determinante;
+				}		
+			}
+		}
+		return -0.71;
+	}	
 }
