f33b0035e208add5bc84b10b41cca1d1d5b68441
Yasmin Farias Romi de Sant'Anna <yasmin.romi@al.infnet.edu.br>
2015-05-29 19:17:23 -0300

    Modulo Vetor v1.3
    Adicionei construtores para a classe ModuloVetor, introduzi o set&get de
    modulo e comentei as classes ModuloVetor e ModuloVetorTeste

--- src/com/calculadora/teste/vetores/ModuloVetorTeste.java
+++ src/com/calculadora/teste/vetores/ModuloVetorTeste.java
@@ -1,24 +1,27 @@
+// ModuloVetorTeste.java
 package com.calculadora.teste.vetores;
 
+// para o teste ser realizado precisarei importar a classe ModuloVetor
 import com.calculadora.vetores.ModuloVetor;
 
-import java.util.Scanner;
-
+// Testa a classe ModuloTeste
 public class ModuloVetorTeste {
 
 	public static void main(String[] args) {
 		
-		ModuloVetor moduloVetor = new ModuloVetor();
-		
 		double[] vetor = {3, 4, 0};
 		
-		moduloVetor.setVetor(vetor);
+		// cria uma constante referente ao resultado esperado
+		final double RESULTADO_ESPERADO = 5;
 		
-		if(moduloVetor.calculaModulo() == 5)
+		// cria um obejto com o construtor com um parâmetro  
+		ModuloVetor moduloVetor = new ModuloVetor(vetor);
+		
+		// checa se o calculo ocorreu como o esperado
+		if(moduloVetor.calculaModulo() == RESULTADO_ESPERADO)
 			System.out.println("Teste realizado com sucesso!");
-	
 		else 
-			System.out.println("Sinto muito, nao deu certo :'(");	
+			System.out.println("Sinto muito, nao deu certo.");	
 
 	}
 	
--- src/com/calculadora/vetores/ModuloVetor.java
+++ src/com/calculadora/vetores/ModuloVetor.java
@@ -1,7 +1,11 @@
+// ModuloVetor.java
 package com.calculadora.vetores;
 
+//importa a classe Scanner para ler numeros 
 import java.util.Scanner;
 
+// calcula o módulo de um vetor
+// formula: raiz((v[0] * v[0]) + (v[1] * v[1]) + (v[2] * v[2]))
 public class ModuloVetor {
 	final int MAX_INDEX = 3;
 	
@@ -10,41 +14,81 @@ public class ModuloVetor {
 	private double modulo; 
 
 	Scanner input = new Scanner(System.in); 
-	 
+	
+	// Cria um construtor simples
+	public ModuloVetor() {
+		
+	}
+	
+	// Cria um construtor que obtem um vetor
+	public ModuloVetor (double[] vetor) {
+		this.vetor = vetor;
+	}
+	
+	// obtem o valor de vetor
 	public void setVetor(double[] vetor) {
 		  this.vetor = vetor;	 
 	 } 
 	 
+	// retorna o valor de vetor
 	 public double[] getVetor() {
 		 return vetor;
 	 }
 	 
-	 public double[] leValores() {
-		 
-		 for(int index = 0; index < MAX_INDEX; index++) {
-				System.out.print("Insira o elemento: ");
-				vetor[index] = input.nextInt();
-			}
-		 return vetor;
+	 // obtem o valor de modulo
+	 public void setModulo(double modulo) {
+		 this.modulo = modulo;
 	 }
 	 
-	 public double calculaModulo() {
-		 
-		 this.getVetor();
-		 
-		 modulo = Math.sqrt((vetor[0]*vetor[0]) + (vetor[1]*vetor[1]) + (vetor[2]*vetor[2]));
-		 
+	 // retorna o valor de modulo
+	 public double getModulo() {
 		 return modulo;
 	 }
-
-	 public void mostraModulo() {
-		 System.out.println("O módulo do vetor eh:" + modulo);
+	 
+	 // le os valores de um vetor
+	 public double[] leValores() {
+		 double[] vetorAux = new double[MAX_INDEX];
+		 
+		 // para cada indice o vetor auxiliar obtem o valor que será imputado
+		 for(int index = 0; index < MAX_INDEX; index++) {
+				System.out.print("Insira o elemento: ");
+				vetorAux[index] = input.nextDouble();
+			}
+		 // o vetor obtem o valor de vetor auxiliar
+		 this.setVetor(vetorAux);
+		 
+		 // retorna o valor do vetor
+		 return this.getVetor();
 	 }
 	 
+	 // Faz e retorna o calculo do modulo de um vetor 
+	 public double calculaModulo() {
+		 // cria um numero auxiliar 
+		 double moduloAux = 0;
+		 
+		 // cria vetor auxiliar que obtem o valor do vetor a ser calculado 
+		 double[] vetorAux = getVetor();
+		 
+		 // calcula a formula do módulo de um vetor
+		 moduloAux = Math.sqrt((vetorAux[0] * vetorAux[0]) + (vetorAux[1] * vetorAux[1]) + (vetorAux[2] * vetorAux[2]));
+		 
+		 // modulo obtem o valor de moduloAux
+		 this.setModulo(moduloAux);
+		 
+		 return moduloAux;
+	 }
+	 
+	 // printa na tela o resultado do modulo
+	 public void mostraModulo() {
+		 System.out.println("O módulo do vetor eh:" + this.getModulo());
+	 }
+	 
+	 // insere os valores de um vetor, calcula e printa na tela o resultado
 	 public void modulo() {
+		 
 		 this.setVetor(this.leValores());
 		 this.calculaModulo();
 		 this.mostraModulo();
 	 }
 	 
-}
+} // termino da classe ModuloVetor
