8255d83a38317a1e155793c86f644fd1a1be0f1c
tiagohs <tiago.silva.93@hotmail.com>
2015-05-30 17:45:59 -0300

    OperacoesBasicas v1.2
    Operações Básicas passadas para BigDecimal.

--- src/com/calculadora/calculadora/OperacoesBasicas.java
+++ src/com/calculadora/calculadora/OperacoesBasicas.java
@@ -1,53 +1,84 @@
 package com.calculadora.calculadora;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
+/**
+ * @author Tiago
+ *	Operações Básicas de Soma, Multiplicação, Subtração e Divisão
+ */
+
 public class OperacoesBasicas {
-	private double valorA;
-	private double valorB;
+	private BigDecimal valorA;
+	private BigDecimal valorB;
+	private BigDecimal resultado;
 	
-	public double getValorA() {
+	//Gets e Sets para Leitura dos Valores
+	public BigDecimal getValorA() {
 		return valorA;
 	}
 
-	public void setValorA(double valorA) {
+	public void setValorA(BigDecimal valorA) {
 		this.valorA = valorA;
 	}
 
-	public double getValorB() {
+
+	public BigDecimal getValorB() {
 		return valorB;
 	}
 
-	public void setValorB(double valorB) {
+
+	public void setValorB(BigDecimal valorB) {
 		this.valorB = valorB;
 	}
 
-	public void lerValores() {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Entre com o Valor de A: ");
-		setValorA(input.nextDouble());
-		System.out.print("Entre com o Valor de B: ");
-		setValorB(input.nextDouble());
+	public BigDecimal getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(BigDecimal resultado) {
+		this.resultado = resultado;
 	}
 	
+	//Leitura dos Valores, passando por parâmetro os Dados como BigDecimal
+	public void lerValores() {
+		Scanner input = new Scanner(System.in);
+		BigDecimal leituraValores;
+		
+		System.out.print("Entre com o Valor de A: ");
+		leituraValores = new BigDecimal(input.nextLine());
+		setValorA(leituraValores);
+		System.out.print("Entre com o Valor de B: ");
+		leituraValores = new BigDecimal(input.nextLine());
+		setValorB(leituraValores);
+	}
+	
+	//Operações, com retorno de um valor Double
 	public double operacaodeSoma() {
-		return (getValorA() + getValorB());
+		setResultado(getValorA().add(getValorB()));
+		return (getResultado().doubleValue());
 	}
 	
 	public double operacaodeSubtracao() {
-		return (getValorA() - getValorB());
+		setResultado(getValorA().subtract(getValorB()));
+		return (getResultado().doubleValue());
 	}
 	
 	public double operacaodeMultiplicacao() {
-		return (getValorA() * getValorB());
+		setResultado(getValorA().multiply(getValorB()));
+		return (getResultado().doubleValue());
 	}
 	
+	//Na Operação Divisão, retorna o valor, com 2 casas decimais pós vírgula,
+	//e arredondamento para cima, se não fizer isso, dá uma excessão
 	public double operacaodeDivisao() {
-		return (getValorA() / getValorB());
+		setResultado(getValorA().divide(getValorB(),2,RoundingMode.UP));
+		return (getResultado().doubleValue());
 	}
 	
-	public double raizQuadrada() {
-		return Math.sqrt(getValorA());
+	//Método para se recuperar o resultado direto em Double, sem a necessidade de conta
+	public double getResultadoDouble() {
+		return (getResultado().doubleValue());
 	}
 }
--- src/com/calculadora/calculadora/OperacoesBasicasMenu.java
+++ src/com/calculadora/calculadora/OperacoesBasicasMenu.java
@@ -2,13 +2,17 @@ package com.calculadora.calculadora;
 
 import java.util.Scanner;
 
+/**
+ * @author Tiago
+ *
+ */
+
 public class OperacoesBasicasMenu {
 	private static final int SAIR = 0;
 	private static final int SOMA = 1;
 	private static final int SUBTRACAO = 2;
 	private static final int MULTIPLICACAO = 3;
-	private static final int DIVISAO = 5;
-	private static final int RAIZ_QUADRADA = 6;
+	private static final int DIVISAO = 4;
 	
 	public void menu() {
 		Scanner input = new Scanner(System.in);
@@ -26,23 +30,18 @@ public class OperacoesBasicasMenu {
 		while (!sair) {
 			switch (input.nextInt()) {
 			case SOMA:
-				System.out.println("\n**Soma**\n");
 				operacaoBasica.lerValores();
 				System.out.printf("Resultado: %.2f", operacaoBasica.operacaodeSoma());
 				break;
 			case SUBTRACAO:
-				System.out.println("\n**Subtracao**\n");
 				operacaoBasica.lerValores();
 				System.out.printf("Resultado: %.2f", operacaoBasica.operacaodeSubtracao());
-
 				break;
 			case MULTIPLICACAO:
-				System.out.println("\n**Multiplicacao**\n");
 				operacaoBasica.lerValores();
 				System.out.printf("Resultado: %.2f", operacaoBasica.operacaodeMultiplicacao());
 				break;
 			case DIVISAO:
-				System.out.println("\n**Divisao**\n");
 				operacaoBasica.lerValores();
 				System.out.printf("Resultado: %.2f", operacaoBasica.operacaodeDivisao());
 				break;
