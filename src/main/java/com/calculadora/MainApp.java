6e03325a58e7f2b58808911d31f7c20c8ba2e02f
tiagohs <tiago.silva.93@hotmail.com>
2016-06-11 19:47:05 -0300

    Tela de Welcome Criada.

--- src/main/java/com/calculadora/MainApp.java
+++ src/main/java/com/calculadora/MainApp.java
@@ -2,11 +2,12 @@ package com.calculadora;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.calculadora.config.ConfigProperties;
 import com.calculadora.controller.CalculadoraController;
 import com.calculadora.controller.ConversoesController;
-import com.calculadora.controller.EscolherIdiomaController;
 import com.calculadora.controller.FracoesController;
 import com.calculadora.controller.OpcoesController;
 import com.calculadora.controller.PorcentagensController;
@@ -17,6 +18,7 @@ import com.calculadora.controller.SobreController;
 import com.calculadora.util.Idioma;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -25,13 +27,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
 	public static final String CAMINHO_ICONE_APLICACAO = "/images/logo.png";
 
 	private ConfigProperties label;
-
-	private Stage escolherIdiomaStage;
+	private int currentSegundos = 0;
+	
 	private Stage rootStage;
 	private Stage opcoesStage;
 	private Stage sobreStage;
@@ -41,12 +44,10 @@ public class MainApp extends Application {
 	private Stage primosStage;
 	private Stage regraTresStage;
 
-	private AnchorPane escolherIdiomaLayout;
 	private BorderPane rootLayout;
 	
 	private RootLayoutController rootLayoutController;
 	private CalculadoraController calculadoraController;
-	private EscolherIdiomaController escolherIdiomaController;
 	private OpcoesController opcoesController;
 	private SobreController sobreController;
 	private ConversoesController conversoesController;
@@ -57,10 +58,32 @@ public class MainApp extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		this.escolherIdiomaStage = primaryStage;
-		this.escolherIdiomaStage.setTitle("Escolher Idioma");
+		label = ConfigProperties.getInstance(Idioma.Portugues);
+		initWelcome(primaryStage);
+	}
+	
+	private void initWelcome(Stage primaryStage) {
+		FXMLLoader loader = new FXMLLoader();
+		AnchorPane welcome = (AnchorPane) getLayout(loader, "/views/welcome.fxml");
+		
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setScene(new Scene(welcome));
+		primaryStage.show();
+		
+		new Timer().schedule(new TimerTask() {
+			public void run() {
+				Platform.runLater(new Runnable() {
+					public void run() {
+						currentSegundos++;
 
-		initEscolherIdioma();
+						if (currentSegundos == 3) {
+							primaryStage.close();
+							initRoot();
+						}
+					}
+				});
+			}
+		}, 0, 1000);
 	}
 	
 	public FXMLLoader getLoader() {
@@ -80,7 +103,7 @@ public class MainApp extends Application {
 		}
 	}
 	
-	public Stage getStage(FXMLLoader loader, Parent pageLayout, String nomeAplicacao, String icone) {
+	public Stage getStage(Parent pageLayout, String nomeAplicacao, String icone) {
 		
 		Stage novoStage = new Stage();
 		novoStage.initModality(Modality.WINDOW_MODAL);
@@ -91,25 +114,12 @@ public class MainApp extends Application {
 		
 		return novoStage;
 	}
-
-	public void initEscolherIdioma() {
-
-		FXMLLoader loader = new FXMLLoader();
-		escolherIdiomaLayout = (AnchorPane) getLayout(loader, "/views/EscolherIdioma.fxml");
-
-		escolherIdiomaStage.setScene(new Scene(escolherIdiomaLayout));
-		escolherIdiomaStage.getIcons().add(new Image(CAMINHO_ICONE_APLICACAO));
-		escolherIdiomaStage.show();
-
-		escolherIdiomaController = loader.getController();
-		escolherIdiomaController.show(escolherIdiomaStage, this);
-	}
 	
 	public void initRoot() {
 		
 		FXMLLoader loader = getLoader();
 		rootLayout = (BorderPane) getLayout(loader, "/views/RootLayout.fxml");
-		rootStage = getStage(loader, rootLayout, label.getString("root.titulo"), CAMINHO_ICONE_APLICACAO);
+		rootStage = getStage(rootLayout, label.getString("root.titulo"), CAMINHO_ICONE_APLICACAO);
 		rootStage.show();
 		
 		rootLayoutController = loader.getController();
@@ -127,7 +137,7 @@ public class MainApp extends Application {
 	public void initOpcoes() {
 		
 		FXMLLoader loader = getLoader();
-		opcoesStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/Opcoes.fxml"), label.getString("opcoes.titulo"), CAMINHO_ICONE_APLICACAO);
+		opcoesStage = getStage((AnchorPane) getLayout(loader, "/views/Opcoes.fxml"), label.getString("opcoes.titulo"), CAMINHO_ICONE_APLICACAO);
 		opcoesStage.show();
 		
 		opcoesController = loader.getController();
@@ -137,7 +147,7 @@ public class MainApp extends Application {
 	public void initSobre() {
 		
 		FXMLLoader loader = getLoader();
-		sobreStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/Sobre.fxml"), label.getString("sobre.tituloJanela"), CAMINHO_ICONE_APLICACAO);
+		sobreStage = getStage((AnchorPane) getLayout(loader, "/views/Sobre.fxml"), label.getString("sobre.tituloJanela"), CAMINHO_ICONE_APLICACAO);
 		sobreStage.show();
 		
 		sobreController = loader.getController();
@@ -147,7 +157,7 @@ public class MainApp extends Application {
 	public void initConversoes() {
 		
 		FXMLLoader loader = getLoader();
-		conversoesStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/outrasOperacoes/Conversoes.fxml"), label.getString("root.tab.arquivo.conversoes.titulo"), CAMINHO_ICONE_APLICACAO);
+		conversoesStage = getStage((AnchorPane) getLayout(loader, "/views/outrasOperacoes/Conversoes.fxml"), label.getString("root.tab.arquivo.conversoes.titulo"), CAMINHO_ICONE_APLICACAO);
 		conversoesStage.show();
 		
 		conversoesController = loader.getController();
@@ -157,7 +167,7 @@ public class MainApp extends Application {
 	public void initFracoes() {
 		
 		FXMLLoader loader = getLoader();
-		fracoesStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/outrasOperacoes/Fracoes.fxml"), label.getString("root.tab.arquivo.fracoes.titulo"), CAMINHO_ICONE_APLICACAO);
+		fracoesStage = getStage((AnchorPane) getLayout(loader, "/views/outrasOperacoes/Fracoes.fxml"), label.getString("root.tab.arquivo.fracoes.titulo"), CAMINHO_ICONE_APLICACAO);
 		fracoesStage.show();
 		
 		fracoesController = loader.getController();
@@ -167,7 +177,7 @@ public class MainApp extends Application {
 	public void initPorcentagens() {
 		
 		FXMLLoader loader = getLoader();
-		porcentagensStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/outrasOperacoes/Porcentagens.fxml"), label.getString("root.tab.arquivo.porcentagem.titulo"), CAMINHO_ICONE_APLICACAO);
+		porcentagensStage = getStage((AnchorPane) getLayout(loader, "/views/outrasOperacoes/Porcentagens.fxml"), label.getString("root.tab.arquivo.porcentagem.titulo"), CAMINHO_ICONE_APLICACAO);
 		porcentagensStage.show();
 		
 		porcentagensController = loader.getController();
@@ -177,7 +187,7 @@ public class MainApp extends Application {
 	public void initPrimos() {
 		
 		FXMLLoader loader = getLoader();
-		primosStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/outrasOperacoes/Primos.fxml"), label.getString("root.tab.arquivo.primo.titulo"), CAMINHO_ICONE_APLICACAO);
+		primosStage = getStage((AnchorPane) getLayout(loader, "/views/outrasOperacoes/Primos.fxml"), label.getString("root.tab.arquivo.primo.titulo"), CAMINHO_ICONE_APLICACAO);
 		primosStage.show();
 		
 		primosController = loader.getController();
@@ -187,7 +197,7 @@ public class MainApp extends Application {
 	public void initRegraTres() {
 		
 		FXMLLoader loader = getLoader();
-		regraTresStage = getStage(loader, (AnchorPane) getLayout(loader, "/views/outrasOperacoes/RegraTres.fxml"), label.getString("root.tab.arquivo.regratres.titulo"), CAMINHO_ICONE_APLICACAO);
+		regraTresStage = getStage((AnchorPane) getLayout(loader, "/views/outrasOperacoes/RegraTres.fxml"), label.getString("root.tab.arquivo.regratres.titulo"), CAMINHO_ICONE_APLICACAO);
 		regraTresStage.show();
 		
 		regraTresController = loader.getController();
